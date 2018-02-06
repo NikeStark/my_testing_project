86d1e3024ff710484f96fdd7369cc383b8fc5e9f
Aliaksandr Kazutsin <kazutin.1993@mail.ru>
2018-02-06 17:15:23 +0300

    js

---
+++ test.js
@@ -0,0 +1,24 @@
+//Циклы
+
+
+//Массивы
+var colors = new Array ("Red","Green","Grey","Blue");
+colors.push ("Yellow");
+document.write(colors[4]+"<br>");
+
+//Математические операции и переменные;
+var FirstNum = 55;
+var SecondNum = 45;
+var ThirdNum = 100;
+FirstNum -=5;
+ThirdNum++;
+document.write (Math.PI +"<br>");
+document.write (Math.round (2.8) +"<br>");
+document.write ("Ваш результат: ",FirstNum + SecondNum - ThirdNum);
+var num = "Hello world!";
+var Num = 2;
+console.log (num);
+
+//Comments
+//var name = prompt ("What your name?");
+//console.log ("My name is " + name);
